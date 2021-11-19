29cf171dfe515b56743f766a0eee6481bf37f967
Mehdi Amrane <91608492+Mehdiamrane08@users.noreply.github.com>
2021-11-19 11:27:00 +0100

    Add files via upload

---
+++ ProjetQRcode/QRcode.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+	$(".sidebar-menu > li.have-children a").on("click", function(i){
+		  i.preventDefault();
+    if( ! $(this).parent().hasClass("active") ){
+      $(".sidebar-menu li ul").slideUp();
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+      $(this).parent().addClass("active");
+    }
+    else{
+      $(this).next().slideToggle();
+      $(".sidebar-menu li").removeClass("active");
+        }
+    });
+});
+
+
+
+var sidebar = document.getElementById("side-bar")
+var hamburger = document.getElementById("hamburger")
+var laSidebarEstAffiche = true 
+hamburger.addEventListener("click", function() {
+  if (laSidebarEstAffiche == true) {
+     sidebar.classList.add("active-side")
+     laSidebarEstAffiche = false
+   }  
+  else {
+    sidebar.classList.remove("active-side")
+    laSidebarEstAffiche = true
+  }
+  
+})
+
+
+
+
+
+
+
+
